14d551270ab02b92a2aef11390d8a19995a60a78
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-17 18:48:20 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
+
+Fido = Dog.new
+Fido.name
+Fido.breed  
